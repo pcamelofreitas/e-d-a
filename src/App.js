264f2fd79478cd5f53dcf4f0a5f5b242fb8993fd
Pedro Freitas <pcamelofreitas@gmail.com>
2022-02-28 17:02:43 -0300

    small changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 
 import "react-bootstrap";
 import "./App.css";
@@ -23,7 +23,7 @@ function d6() {
 
 const App = () => {
   //states
-  const [loginState, setLoginState] = useState(false);
+  // const [loginState, setLoginState] = useState(false);
   //route
   const [route, setRoute] = useState("signin");
   // refrences----
@@ -56,12 +56,14 @@ const App = () => {
   };
 
   // add and remove items ------------------
+  //add
   const addItemHandler = (item) => {
     setItems((prevItems) => [...prevItems, item]);
   };
-  const removeItemHandler = (itemId) => {
-    setItems((prevItems) => prevItems.filter((item) => item.id !== itemId));
-  };
+  //rmv
+  // const removeItemHandler = (itemId) => {
+  //   setItems((prevItems) => prevItems.filter((item) => item.id !== itemId));
+  // };
   const onRouteChange = (string) => {
     setRoute(string);
   };
@@ -174,140 +176,3 @@ export default App;
 //     </section>
 //   );
 // };
-
-// class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       //estado de log
-//       isSingIn: false,
-//       //rota
-//       route: "signin",
-//       //seção
-//       id: "historico",
-//       //personagem
-//       personagem: {
-//         tempo: 0,
-//         energia: 0,
-//         habilidade: 0,
-//         sorte: 4,
-//         ouro: 0,
-//         joias: 0,
-//         pocoes: 5,
-//         provisoes: 5,
-//         bolsa: ["gato", "cachorro", "papagaio"],
-//       },
-//       //sorte
-//       sortudo: false,
-//     };
-//   }
-//   //rolagem de dados
-
-//   //avanço de paginas
-//   goToRef = (event) => {
-//     this.setState({ id: event });
-//   };
-//   //teste de sorte
-//   testeSorte = () => {
-//     let teste = d6();
-//     if (teste > this.state.personagem.sorte) {
-//       this.setState({ sortudo: true });
-//     } else {
-//       this.setState({ sortudo: false });
-//     }
-//   };
-
-//   //Criação de personagem
-//   createHab = (h) => {
-//     this.setState({ personagem: { habilidade: 6 + h } });
-//   };
-//   createEne = (e) => {
-//     this.setState({ personagem: { energia: 12 + e } });
-//   };
-//   createSor = (s) => {
-//     this.setState({ personagem: { sorte: 6 + s } });
-//   };
-
-//   // mudança de rota
-//   onRouteChange = (string) => {
-//     this.setState({
-//       route: string,
-//     });
-//   };
-
-//   render() {
-//     const { route, personagem, id, sortudo } = this.state;
-
-//     return (
-//       <div>
-//         {/* condicional de rota */}
-//         {route === "home" ? (
-//           <div>
-//             {/* barra de navegação */}
-//             <Navigation
-//               onrRouteChange={this.onRouteChange}
-//               personagem={personagem}
-//             />
-//             <div className="container text-center">
-//               <References
-//                 id={id}
-//                 goToRef={this.goToRef}
-//                 testSorte={this.testeSorte}
-//                 sortudo={sortudo}
-//               />
-//             </div>
-//             {/* <Referencia goToRef={this.goToRef} id={id} /> */}
-//             {/* <Ref1 goToRef={this.goToRef} /> */}
-
-//             {/* conteudo */}
-//             {/* <div className='row d-inline text-center'>
-//                             <div  className='col vh-75'>
-//                                 <h2 className='f1'>{conteudo[id].ref}</h2>
-//                                 <img alt="imagem-da-sec" src={conteudo[id].img} height="250px"/>
-//                                 <p>{conteudo[id].texto}</p>
-//                             </div>
-//                         </div> */}
-
-//             {/* barra de botoes */}
-//             {/* <div className='row  h-25 w-100 d-inline-block '>
-//                             <div className='d-flex justify-content-around '>
-//                                 {conteudo[this.state.id].btns.map((btn, index) =>
-//                                     <Button
-//                                     variant="dark"
-//                                     key={index}
-//                                     onClick={this.goToRef.bind(this, btn)}
-//                                     >ir para  {btn}
-//                                     </Button>
-//                                 )}
-//                             </div>
-//                         </div> */}
-//           </div>
-//         ) : route === "signin" ? (
-//           // seção de login
-//           <div>
-//             <div className="bg-dark text-white fs-3 text-center vw-100">
-//               <p>Escravos do Abismo</p>
-//             </div>
-//             <Signin onRouteChange={this.onRouteChange} />
-//           </div>
-//         ) : route === "register" ? (
-//           // registro
-//           <Register onRouteChange={this.onRouteChange} />
-//         ) : (
-//           <Create
-//             onRouteChange={this.onRouteChange}
-//             personagem={personagem}
-//             createEne={this.createEne}
-//             createHab={this.createHab}
-//             createSor={this.createSor}
-//           />
-//         )}
-//         {/* footer */}
-//         <div className="d-flex justify-content-end position-fixed bottom-0 end-0 bg-dark w-100">
-//           <Footer onRouteChange={this.onRouteChange} />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-// export default App;
